5031392c27e7a3772edc7c012407860b9dfa7353
privs18 <priyarathi2003@gmail.com>
2024-09-11 00:04:45 +0530

    instrumented tests fixed

--- app/src/androidTest/java/androidsamples/java/dicegames/WalletInstrumentedTest.java
+++ app/src/androidTest/java/androidsamples/java/dicegames/WalletInstrumentedTest.java
@@ -36,32 +36,27 @@ public class WalletInstrumentedTest {
   @Test
     public void testWalletViewModelBalance() {
         WalletViewModel walletViewModel = new WalletViewModel();
-        int bal = walletViewModel.balance();
-        assertEquals(bal+1, walletViewModel.balance());
+        assertEquals(0, walletViewModel.balance());
     }
   @Test
     public void testWalletViewModelTotalRolls() {
         WalletViewModel walletViewModel = new WalletViewModel();
-        int rolls = walletViewModel.totalRolls();
-        assertEquals(rolls+1, walletViewModel.totalRolls());
+        assertEquals(0, walletViewModel.totalRolls());
     }
     @Test
     public void testWalletViewModelSingleSixes() {
         WalletViewModel walletViewModel = new WalletViewModel();
-        int six = walletViewModel.singleSixes();
-        assertEquals(six+1, walletViewModel.singleSixes());
+        assertEquals(0, walletViewModel.singleSixes());
     }
     @Test
     public void testWalletViewModelDoubleSixes() {
         WalletViewModel walletViewModel = new WalletViewModel();
-        int dSixes = walletViewModel.doubleSixes();
-        assertEquals(dSixes+1, walletViewModel.doubleSixes());
+        assertEquals(0, walletViewModel.doubleSixes());
     }
     @Test
     public void testWalletViewModelDoubleOthers() {
         WalletViewModel walletViewModel = new WalletViewModel();
-        int dOthers = walletViewModel.doubleOthers();
-        assertEquals(dOthers+1, walletViewModel.doubleOthers());
+        assertEquals(0, walletViewModel.doubleOthers());
     }
 
 }
